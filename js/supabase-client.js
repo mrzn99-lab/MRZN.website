f68861545aaeef7668e8099b03f219f02d4c3387
MRZN apps games <mdrafiuzzamanking99@gmail.com>
2026-08-25 07:51:50 +0600

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://qweyjpqxvixyzoremhon.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3ZXlqcHF4dml4eXpvcmVtaG9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMTIzOTYsImV4cCI6MjA5OTc4ODM5Nn0.Bntl040U9kmWm2YmtJYcqUZ3n7SkhfOb581FTh-3xt4";
 const ADMIN_EMAILS = ["mdrafiuzzamanking99@gmail.com"];
-// RIGHT - এভাবে হওয়া উচিত
+
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
